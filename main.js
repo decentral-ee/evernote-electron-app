70f36aae6b73a9a32b159cf511fcc8afda830bb8
Miao ZhiCheng <miao@decentral.ee>
2019-01-06 14:30:26 +0200

    support outbound url redirect

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 const electron = require('electron');
 const { shell, app, BrowserWindow } = electron;
-const HOMEPAGE = 'https://www.evernote.com/Home.action'
+const HOMEPAGE = 'https://www.evernote.com/Home.action';
+const OUTBOUND_REDIRECT_URL = 'https://www.evernote.com/OutboundRedirect.action?dest=';
 
 let mainWindow;
 
 app.on('ready', () => {
-    window = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1200,
         height: 900,
         webPreferences: {
           nodeIntegration: false
         }
     });
-    window.setMenuBarVisibility(false);
-    window.loadURL(HOMEPAGE);
+    win.setMenuBarVisibility(true);
+    win.loadURL(HOMEPAGE);
 
-    window.webContents.on('will-navigate', (ev, url) => {
+    win.webContents.on('will-navigate', (ev, url) => {
         if (!url.startsWith("https://www.evernote.com/") && 
             !url.startsWith("https://accounts.google.com/")) {
             ev.preventDefault();
             shell.openExternal(url);
-        };
+        }
     });
 
-    window.on('closed', () => {
-        window = null;
+    win.webContents.on('new-window', (ev, url) => {
+        console.debug("new-window", url);
+        if (url.startsWith(OUTBOUND_REDIRECT_URL)) {
+            ev.preventDefault();
+            let outboundUrl = decodeURIComponent(url.substring(OUTBOUND_REDIRECT_URL.length));
+            console.debug("Redirect to outbound", outboundUrl);
+            shell.openExternal(outboundUrl);
+        }
+    });
+
+    win.on('closed', () => {
+        win = null;
         app.quit();       
     });
 });
